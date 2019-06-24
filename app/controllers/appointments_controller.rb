a08edc7fb0080402f7ca6e3ed32b1c27f617f32d
José Gabriel González <jgg032899@gmail.com>
2019-06-24 14:35:26 -0400

    Fixes (Edillio)

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -11,14 +11,14 @@ class AppointmentsController < ApiController
     if @appointment.save
       render json: {message: "La cita ha sido creada"}, status: :ok
     else
-      render json: {errors: @appointment.errors.full_messages}
+      render json: {error: true, messages:@appointment.errors.full_messages}
     end
   end
 
   def update
     @appointment = Appointment.find_by_id(params[:id])
     if @appointment.nil?
-      render json: {errors: "La cita no existe"}
+      render json: {error: true, messages:"La cita no existe"}
     else
       if @appointment.update_attributes(appointment_params)
         render json: {message: "Se han actualizado los datos de la cita exitosamente!"}
@@ -31,7 +31,7 @@ class AppointmentsController < ApiController
   def delete
     @appointment = Appointment.find_by_id(params[:id])
     if @appointment.nil?
-      render json: {errors: "La cita no existe"}
+      render json: {error: "La cita no existe"}
     else
       if @appointment.destroy
         render json: {message: "La cita se ha eliminado exitosamente"}
@@ -44,6 +44,6 @@ class AppointmentsController < ApiController
   private
 
   def appointment_params
-    params.permit(:appointment_date, hour_date, :motive, doctor_id, secretary_id, patient_id)
+    params.permit(:appointment_date, :hour_date, :motive, :doctor_id, :secretary_id, :patient_id)
   end
 end
