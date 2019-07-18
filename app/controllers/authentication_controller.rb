89dc6a3fd85c2e427565760bb940f4b7f0872b0c
José Gabriel González <jgg032899@gmail.com>
2019-07-18 14:07:17 -0400

    Arreglé un bug en el Login.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,6 +7,11 @@ class AuthenticationController < ApiController
     end
 
     @user = User.joins(:person).where(people: {email: params[:email]}).first
+
+    if @user.nil?
+      return render status: 200, json: {error: true, message: 'Datos incorrectos'}
+    end
+
     if @user.authenticate(params[:password])
       token = JsonWebToken.encode({user_id: @user.id, name: @user.person.name, last_name: @user.person.last_name,
                                    email: @user.person.email, role: @user.role.name, specialty: (@user.specialty.name unless @user.specialty.nil?)})
