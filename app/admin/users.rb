80868b76c18fac028e7e3ef1d0efbbcec0600667
José Gabriel González <jgg032899@gmail.com>
2019-08-01 21:08:20 -0400

    Se pueden crear usuarios con constraseña desde el admin (#26)

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,19 @@
 ActiveAdmin.register User do
   permit_params :person_id, :specialty_id, :role_id, :work_day_id,
                 :entry_time, :departure_time, :password, :password_confirmation
+
+  form do |f|
+    f.inputs do
+      f.semantic_errors
+      f.input :person_id, :as => :select, :collection => Person.all.map { |u| [u.display_name, u.id] }
+      f.input :specialty_id, :as => :select, :collection => Specialty.all.map { |u| [u.display_name, u.id] }
+      f.input :role_id, :as => :select, :collection => Role.all.map { |u| [u.display_name, u.id] }
+      f.input :work_day_id, :as => :select, :collection => WorkDay.all.map { |u| [u.display_name, u.id] }
+      f.input :entry_time
+      f.input :departure_time
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
